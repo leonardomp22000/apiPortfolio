95fdd3396869408972dd57d71d19cd966bc6ca30
Leonardo <leonardomp2200@gmail.com>
2025-06-28 14:52:46 -0600

    debug code

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const { exec } = require("child_process");
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// app.get("/start-hand-tracking", (req, res) => {
-//   // Ejecuta el script de Python
-//   const pythonProcess = exec(
-//     "python3 hand_tracking.py",
-//     (error, stdout, stderr) => {
-//       if (error) {
-//         console.error(`Error: ${error.message}`);
-//         return res.status(500).json({ error: error.message });
-//       }
-//       if (stderr) {
-//         console.error(`stderr: ${stderr}`);
-//         return res.status(500).json({ error: stderr });
-//       }
-//       res.json({ message: stdout });
-//     }
-//   );
-
-//   res.json({ message: "Hand tracking started" });
-// });
-
-// app.listen(5000, () => console.log("Server running on port 5000"));
-
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
@@ -40,8 +10,7 @@ const io = new Server(server, {
   cors: { origin: "*" },
 });
 
-const PYTHON_API_URL =
-  process.env.PYTHON_API_URL || "https://pythonopencvserve.onrender.com"; // http://127.0.0.1:5001
+const PYTHON_API_URL = process.env.PYTHON_API_URL || "http://127.0.0.1:5001"; // http://127.0.0.1:5001
 
 io.on("connection", (socket) => {
   console.log("Cliente conectado");
